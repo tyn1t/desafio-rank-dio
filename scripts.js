ef412bb55fa00c9b6e0be4c2471b8609cc496a45
Giovanni <anderlansouza71@gmail.com>
2024-12-09 22:59:59 -0400

    first commit

--- scripts.js
+++ scripts.js
@@ -4,22 +4,36 @@ function calculadoraPartidasRankeadas(vitorias = 0, derrotas = 0) {
 }
 
 function Rankeadas() {
-    return ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"];
+    return ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"]
 }
 
 let saldoVitorias = calculadoraPartidasRankeadas(93, 2);
-let nivel = Rankeadas();
+let ranks  = Rankeadas();
 
-if (saldoVitorias >= 11 && saldoVitorias <= 20) {
-    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[0]}`);
-} else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
-    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[1]}`);
-} else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
-    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[2]}`); // Corrected index
-} else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
-    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[3]}`); // Corrected index
-} else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
-    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[4]}`); // Corrected index
-} else {
-    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[5]}`); //Corrected index.  Assumed you want Imortal as the default
+const rank = [10, 50, 80, 90, 100];
+
+let rankIndex = 0;
+
+for (let index = 0; index < rank.length; index++) {
+    
+    console.log(rank[index])
+    if (saldoVitorias <= rank[index]) {
+        rankIndex = index
+        break
+   }
 }
+console.log(rankIndex)
+console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${ranks[rankIndex -1]}`);
+
+// if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+//     console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[0]}`);
+// } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+//     console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[1]}`);
+// } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+//     console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[2]}`);
+// } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+//     console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[3]}`); 
+//     console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[4]}`); 
+// } else {
+//     console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel[5]}`);
+// }
